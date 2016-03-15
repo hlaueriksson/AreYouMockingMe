d4459a338844c5cf8011115288e6ce662316140b
Henrik Lau Eriksson <Henrik.Eriksson@liv.local>
2016-03-15 16:59:12 +0100

    first commit
    Tests with:
    :white_check_mark: NUnit + Moq
    :white_check_mark: NUnit + Moq + Moq.AutoMock
    :white_check_mark: Machine.Specifications + FakeItEasy + Machine.Fakes

---
+++ AreYouMockingMe/Tests/MSpec/Given_HelloWorld.cs
@@ -0,0 +1,26 @@
+﻿using Machine.Fakes;
+using Machine.Specifications;
+
+namespace AreYouMockingMe.Tests.MSpec
+{
+    [Subject(typeof(HelloWorld))]
+    public class Given_HelloWorld : WithSubject<HelloWorld>
+    {
+        Establish context = () =>
+        {
+            The<IFoo>().WhenToldTo(x => x.GetFoo()).Return("Hello");
+            The<IBar>().WhenToldTo(x => x.GetBar()).Return(", World!");
+        };
+
+        public class GetMessage
+        {
+            static string _result;
+
+            Because of = () => _result = Subject.GetMessage();
+
+            It should_invoke_IFoo_GetMessage = () => The<IFoo>().WasToldTo(x => x.GetFoo());
+            It should_invoke_IBar_GetMessage = () => The<IBar>().WasToldTo(x => x.GetBar());
+            It should_return_a_concatenated_string_with_messages_from_IFoo_and_IBar = () => _result.ShouldEqual("Hello, World!");
+        }
+    }
+}
---
+++ AreYouMockingMe/Tests/NUnit/Given_HelloWorld_with_AutoMocker/When_GetMessage.cs
@@ -0,0 +1,45 @@
+﻿using Moq;
+using Moq.AutoMock;
+using NUnit.Framework;
+using Should;
+
+namespace AreYouMockingMe.Tests.NUnit.Given_HelloWorld_with_AutoMocker
+{
+    public class When_GetMessage
+    {
+        private AutoMocker _mocker;
+        private HelloWorld _subject;
+
+        [SetUp]
+        public void SetUp()
+        {
+            _mocker = new AutoMocker();
+            _subject = _mocker.CreateInstance<HelloWorld>();
+        }
+
+        [Test]
+        public void Should_invoke_IFoo_GetMessage()
+        {
+            _subject.GetMessage();
+
+            _mocker.GetMock<IFoo>().Verify(x => x.GetFoo());
+        }
+
+        [Test]
+        public void Should_invoke_IBar_GetMessage()
+        {
+            _subject.GetMessage();
+
+            _mocker.GetMock<IBar>().Verify(x => x.GetBar(), Times.Once);
+        }
+
+        [Test]
+        public void Should_return_a_concatenated_string_with_messages_from_IFoo_and_IBar()
+        {
+            _mocker.GetMock<IFoo>().Setup(x => x.GetFoo()).Returns("Hello");
+            _mocker.GetMock<IBar>().Setup(x => x.GetBar()).Returns(", World!");
+
+            _subject.GetMessage().ShouldEqual("Hello, World!");
+        }
+    }
+}
